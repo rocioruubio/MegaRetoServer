4a94560be2cedc274585ae0811252d34a6128707
Rocío <rocioorubio17@gmail.com>
2021-11-18 14:51:54 +0100

    PRIMERA SUBIDA

---
+++ src/main/java/com/ntt/megareto/MegaRetoServer/MegaRetoServerApplication.java
@@ -0,0 +1,13 @@
+package com.ntt.megareto.MegaRetoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaRetoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaRetoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntt/megareto/MegaRetoServer/MegaRetoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntt.megareto.MegaRetoServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaRetoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
